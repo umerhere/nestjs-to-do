8c11069b0f9bedb61f7247b4ffb9109fc116e485
Umar Aslam <itsumeraslam@gmail.com>
2024-07-14 17:43:36 +0500

    chore: add migration script, update Task entity fields, and install necessary libraries

---
+++ create-and-run-migrations.ts
@@ -0,0 +1,35 @@
+require('dotenv').config(); // Load environment variables from .env file
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+console.log('Running migrations in development environment');
+
+const ormConfigPath = path.resolve(__dirname, 'ormconfig.ts');
+
+try {
+    // Ensure migrations folder exists
+    const migrationsFolder = path.resolve(__dirname, 'src', 'migrations');
+    console.log("migrationsFolder ", migrationsFolder);
+    if (!fs.existsSync(migrationsFolder)) {
+        fs.mkdirSync(migrationsFolder, { recursive: true });
+        console.log(`Created migrations folder at ${migrationsFolder}`);
+    }
+
+    console.log("migrationsFolder", migrationsFolder);
+    // Generate a timestamp for the migration name
+    const timestamp = new Date().getTime();
+    const migrationName = `Migration_${timestamp}`;
+
+    // Generate new migration using timestamp as name, specifying migrations directory
+    // execSync(`ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate ${migrationName} -d ${ormConfigPath} -p ${migrationsFolder}/${migrationName}`, { stdio: 'inherit' });
+    execSync(`typeorm-ts-node-esm migration:generate -d ${ormConfigPath} ${migrationsFolder}/${migrationName}`, { stdio: 'inherit' });
+
+
+    // Run migrations
+    execSync(`ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:run -d ${ormConfigPath}`, { stdio: 'inherit' });
+
+    console.log('Migrations ran successfully');
+} catch (error) {
+    console.error('Error running migrations:', error.message);
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,14 @@
-export class Task {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('increment') id: number;
+  @Column({ type: 'varchar', nullable: false }) title: string;
+  @Column({ type: 'varchar', nullable: true }) details: string;
+  @Column({ type: 'varchar', nullable: false }) status: string;
+  @Column({ type: 'varchar', nullable: false }) priority: string;
+  @Column({ type: 'timestamp', nullable: false }) due_date: Date
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' }) date_of_creation: Date
+  @Column({ type: 'boolean', default: false }) is_active: boolean
+  
+}
